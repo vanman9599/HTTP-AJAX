277bc6fac02973f1c5a9544d2f11897f71f49545
van jordan <vanjordan46@hotmail.com>
2019-05-22 13:15:11 -0700

    Initial commit

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FriendsList from 'friends-list';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FriendsList />
+    </div>
+  );
+}
+
+export default App;
